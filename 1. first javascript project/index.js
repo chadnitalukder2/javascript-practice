95c99409176ed22b9db6a411e414377a8921effc
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-09-09 13:13:30 +0600

    Variable

--- 1. first javascript project/index.js
+++ 1. first javascript project/index.js
@@ -1,3 +1,15 @@
-alert("hello");
+/*alert("hello");
 document.write("I love bangladesh");
-console.log("I love bangladesh");
+console.log("I love bangladesh");*/
+/*--------- variables-------------*/
+// var name = "chadni talukder";
+// document.write(name);
+
+// var name;
+// var age;
+var name,age;
+
+name = "sufia";
+age = "20"
+document.write(name);
+document.write(age);
