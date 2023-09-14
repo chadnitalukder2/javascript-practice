d15bff40941193806d3f11b08f0b2b79e17e9f43
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-09-14 16:47:47 +0600

    while loop

--- 1. first javascript project/index.js
+++ 1. first javascript project/index.js
@@ -504,10 +504,44 @@ for(var x = m; x <= n; x = x + 1 ){
 }
 document.write(" sum =" +sum);*/
 
-    for (var x = 1; x <=  5; x++){
+ /*   for (var x = 1; x <=  5; x++){
     var num1 = parseInt(prompt("enter firts number : "));
     var num2 = parseInt(prompt("enter second number : "));
     var sum = num1 + num2;
     document.write("result = " +sum);
 }
+*/
+/*-------------------while loop---------------------*/
+//1+2+4+5........+10
+/*var i =  1;
+var sum = 0;
+while (i <= 10 ) {
+    sum = sum + i;
+  //  document.write("" + i);
+    i = i + 1;
+}
+document.write(sum);
+document.write("<h1>end<h1/>");*/
 
+//2+4+6....+100
+/*var i =  2;
+var sum = 0;
+while (i <= 100 ) {
+    sum = sum + i;
+    i = i + 2;
+}
+document.write(sum);
+document.write("<h1>end<h1/>");*/
+
+//divisible by 3 and 5 from-100
+
+var x = 1;
+var sum = 0;
+while (x <= 100) {
+    if (x%3==0 && x%5==0) {
+        sum = sum + x;
+        document.write(" " + x);
+    }
+    x++;
+}
+document.write(" sum = " + sum);
