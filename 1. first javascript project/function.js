e6915fb2cbfd1722abf43ff160477a32cb48f8d3
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-09-24 16:03:22 +0600

    object-math

--- 1. first javascript project/function.js
+++ 1. first javascript project/function.js
@@ -16,11 +16,11 @@ document.write(x);
 }
 
 //sub function
-function subtraction(x,y) {
-    var result = x - y;
-    document.write("sum = " + result + "<br/>");
-}
-addition(10,20);
+// function subtraction(x,y) {
+//     var result = x - y;
+//     document.write("sum = " + result + "<br/>");
+// }
+addition(1,2,3,4,6,5,7,8,9);
 addition(610,320);
 subtraction(10,20);
 subtraction(610,320);*/
@@ -42,17 +42,44 @@ display2("hi i am message");*/
 
 // Task 7 : create an IIFEs that print sum of 2 numbers
 
-// function findMinimun(a,b,c) {
+/* function findMinimun(a,b,c) {
 
-//     if(a > c && b > c){
-//         return c;
-//     }
-//     else if( a > b && c > b ){
-//         return b;
-//     } else {
-//         return a;
-//     }
+    if(a > c && b > c){
+        return c;
+    }
+    else if( a > b && c > b ){
+        return b;
+    } else {
+        return a;
+    }
 
-// }
+}
 
-// console.log(findMinimun(9,1,1));
+console.log(findMinimun(9,1,1));*/
+
+
+/*function findArray0(arr, num) {
+    for(let i = 0; i < arr.length; i++) {
+        if (arr[i] == num) {
+            console.log("Number gotted", i + 1);
+            console.log("Number is", arr[i]);
+            break;
+        }
+    }
+}
+let numArr = [1, 2, 4, 5, 6, 7, 8,3, 10, 100];
+// findArray(numArr , 10);
+findArray([2,5,6,8,90,10] , 10);*/
+
+
+//addition
+var numbers = [1,2,3,4,5,6,7,8,9,99,7,8,67,80];
+sum = 0
+function AdditionArr (numbers){
+for(var i = 0 ; i <numbers.length; i++){
+    sum = sum + numbers[i];
+}
+}
+AdditionArr(numbers)
+console.log(sum);
+
