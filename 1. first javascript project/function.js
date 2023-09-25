0316fe7ae3dff5b44c243f0126c68e94cb082464
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-09-25 19:04:45 +0600

    guessing-game

--- 1. first javascript project/function.js
+++ 1. first javascript project/function.js
@@ -57,7 +57,6 @@ display2("hi i am message");*/
 
 console.log(findMinimun(9,1,1));*/
 
-
 /*function findArray0(arr, num) {
     for(let i = 0; i < arr.length; i++) {
         if (arr[i] == num) {
@@ -71,15 +70,62 @@ let numArr = [1, 2, 4, 5, 6, 7, 8,3, 10, 100];
 // findArray(numArr , 10);
 findArray([2,5,6,8,90,10] , 10);*/
 
-
 //addition
-var numbers = [1,2,3,4,5,6,7,8,9,99,7,8,67,80];
+/*var products = [1,2,3,4,5,6,7,8,9,99,7,8,67,80];
 sum = 0
 function AdditionArr (numbers){
 for(var i = 0 ; i <numbers.length; i++){
     sum = sum + numbers[i];
 }
 }
-AdditionArr(numbers)
-console.log(sum);
+AdditionArr(products)
+console.log(sum);*/
 
+//sumarry
+/*sum = 0;
+var arr = [
+  {
+    name: "mango",
+    price: 300,
+  },
+  {
+    name: "orange",
+    price: 500,
+  },
+];
+function AdditionArr(numbers) {
+  for (var i = 0; i < numbers.length; i++) {
+    sum = sum + numbers[i].price;
+  }
+}
+AdditionArr(arr);
+
+console.log(sum);*/
+
+//findnumber
+/*var numbers = [
+  1, 2, 3, 4, 5, 6, 7, 31, 45, 32, 56, 37, 63, 23, 9, 8, 76, 54, 24, 55,
+];
+function findNumber(arr, num) {
+  for (var i = 0; i < arr.length; i++) {
+    if (num == arr[i]) {
+      console.log(i);
+    }
+  }
+}
+findNumber(numbers, 5);*/
+
+//odd numbers
+/*var numbers = [
+  1, 2, 3, 4, 5, 6, 7, 31, 45, 32, 56, 37, 63, 23, 9, 8, 76, 54, 24, 55,
+];
+var even = [];
+function findNumber(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+      even.push(arr[i]);
+    }
+  }
+}
+findNumber(numbers);
+console.log(even);*/
---
+++ 1. first javascript project/guessing-game.js
@@ -0,0 +1,18 @@
+var numOfWon = 0;
+var numOfLost = 0;
+for ( var i = 0; i <= 5; i++){ 
+var guessNumber = parseInt(prompt("Enter a number from 1 to 5 : "));
+
+var randomNumber = Math.floor(Math.random()*5) + 1;
+
+if (guessNumber == randomNumber){
+    console.log("You have won");
+    numOfWon++;
+}
+else{
+    console.log("You have lost. Random number was " + randomNumber);
+    numOfLost++;
+}
+}
+document.write("Number of won = " + numOfWon + "<br/>");
+document.write("Number of Lost = " + numOfLost + "<br/>");
