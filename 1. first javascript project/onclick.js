3d0b1a18f5d886efd5bd64c674387f23169b0e2a
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-09-26 09:24:37 +0600

    add piture

---
+++ 1. first javascript project/onclick.js
@@ -0,0 +1,28 @@
+/*function myMessage1(){
+    alert("hello everyone 1")
+}
+function myMessage2(){
+    alert("hello everyone 2")
+}
+function myMessage3(){
+    alert("this is paragraph")
+}*/
+
+//add massage
+/*var myVar = document.querySelector("#peraId");
+function myMessage1(){
+  myVar.innerHTML = "you have clicked on button1"
+}
+function myMessage2(){
+    myVar.innerHTML = "you have clicked on button2"
+  }*/
+ 
+  //add picture
+  var myVar = document.querySelector("#myImgId");
+function myPicture1(){
+  myVar.src ="./img/lemon-tea-bag-300x300.jpg";
+}
+var myVar = document.querySelector("#myImgId");
+function myPicture2(){
+  myVar.src ="./img/groceray.jpg";
+}
