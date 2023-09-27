2955391b2535fb0559c2fbc02bcfd9471d618639
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-09-27 11:49:21 +0600

    audio in javascript

--- 1. first javascript project/img-slider.js
+++ 1. first javascript project/img-slider.js
@@ -81,3 +81,30 @@ myVar.addEventListener("mouseout", function(){
       document.querySelector("h1").innerHTML = text + " is clicked";
     });
 }*/
+
+// how to play audio in javascript
+for (var i = 0; i < 3; i++) {
+  document
+    .querySelectorAll(".myButton")
+    [i].addEventListener("click", function () {
+      var text = this.innerHTML;
+      console.log(text);
+      audioPlay(text);
+    });
+}
+function audioPlay(text) {
+  switch (text) {
+    case "A":
+      var audio = new Audio("sounds/1st.mp3");
+      audio.play();
+      break;
+    case "B":
+      var audio = new Audio("sounds/2nd.mp3");
+      audio.play();
+      break;
+    case "C":
+      var audio = new Audio("sounds/3rd.mp3");
+      audio.play();
+      break;
+  }
+}
