d2c3f0acfcad778a1a7f2c77dac3900aa85d9248
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-09-28 22:44:27 +0600

    checkbox

--- 1. first javascript project/dom-event.js
+++ 1. first javascript project/dom-event.js
@@ -1,6 +1,6 @@
 console.clear();
 
-//const input = document.querySelector('input');
+/*//const input = document.querySelector('input');
 const input = document.querySelector('input[name=name]');
 input.addEventListener("change", changeHandler);
 
@@ -10,10 +10,19 @@ function  changeHandler(e) {
 // console.log(e.target);
 // console.log(e.target.className);
 // console.log(e.target.value);
+}*/
+
+const programs = document.querySelectorAll('input[name=program]');
+console.log(programs);
+
+Array.from(programs).map((program) => {
+program.addEventListener("change",programHandler)
+});
+function programHandler(e){
+if(e.target.checked){
+    console.log(e.target.value);
 }
-
-
-
+};
 
 
 
