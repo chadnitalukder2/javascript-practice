34b4b947da86cddfa3b4cbeeeb13b911fc99dbed
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-09-28 22:23:57 +0600

    change

---
+++ 1. first javascript project/dom-event.js
@@ -0,0 +1,24 @@
+console.clear();
+
+//const input = document.querySelector('input');
+const input = document.querySelector('input[name=name]');
+input.addEventListener("change", changeHandler);
+
+function  changeHandler(e) {
+//  console.log(e);
+// console.log(e.type);
+// console.log(e.target);
+// console.log(e.target.className);
+// console.log(e.target.value);
+}
+
+
+
+
+
+
+
+
+
+
+
