373861ad40e2996c364663ca3235cac8af5b3668
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-09-29 22:55:24 +0600

    BOM-popup boxes

--- 1. first javascript project/BOM.js
+++ 1. first javascript project/BOM.js
@@ -13,7 +13,7 @@ console.log(location.port);
 //pathname
 console.log(location.pathname);*/
 
-var locationDiv = document.querySelector(".location-div");
+/*var locationDiv = document.querySelector(".location-div");
 console.log(locationDiv);
 
 var p1 = locationDiv.children[0];
@@ -25,9 +25,39 @@ p2.textContent = location.protocol;
 const visitButton = document.getElementById("visit-button");
 visitButton.addEventListener("click", function(){
 location.assign("https://www.studywithanis.com")
-});
+});*/
+
+//popup boxes - alert, confirm, prompt
+//alert("error here")
+//confirm("Are you sure");
+
+/*function deleteSomething(){
+    let value = confirm("Do you want to delete?");
+    if(value){
+        console.log("deleted");
+    }
+    else{
+        console.log("not deleted");
+    }
+}
+deleteSomething();*/
 
 
+function welcome(){
+var h1 = document.createElement("h1");
+let text;
+var name = prompt("Enter your name");
+if(name == null || name == ''){
+text = "no name found"
+}
+else{
+    text = "welcome " + name;
+}
+var textNode = document.createTextNode(text);
+h1.appendChild(textNode);
+document.body.appendChild(h1);
+};
+welcome();
 
 
 
