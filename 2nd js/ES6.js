aa804fc0121697647447a5f036eb38256e069917
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-10-01 10:11:26 +0600

    ES6- Arrow1

--- 2nd js/ES6.js
+++ 2nd js/ES6.js
@@ -36,20 +36,36 @@ var squareNumber = numbers.map(function(x){
 });
 console.log(squareNumber);*/
 
-var numbers = [22, 31, 4, 5,26,78,65];
+/*var numbers = [22, 31, 4, 5,26,78,65];
 var newNumber = numbers.filter(function(x){
     return x > 10;
 });
-console.log(newNumber);
+console.log(newNumber);*/
 
+// Arrow function (part-1)------------------------------------------------------------------------------------
+/*function display1(){console.log("I am display1");}
+const display2 = () => console.log("I am display 2");
 
+display1();
+display2();*/
 
+/*function message1(){
+    return "I am message1"
+}
+const message2 = () =>   //akoti line takle secound bracket dewya lage na
+    "I am message1"
 
+console.log(message1());
+console.log(message2());*/
 
+function add1(num1, num2){
+    return num1 + num2;
+}
+const add2 = (num1,num2) => 
+    num1 + num2
 
-
-
-
+console.log(add1(10,20));
+console.log(add2(20,20));
 
 
 
