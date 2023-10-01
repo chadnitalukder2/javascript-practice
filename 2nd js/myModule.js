d0f38dd77884f7e35e60957bcf70d5e021de83a6
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-10-01 13:59:24 +0600

    ES6- Asynchronous

--- 2nd js/myModule.js
+++ 2nd js/myModule.js
@@ -6,7 +6,7 @@ function setText(txt){
 
 
 //class
-class student {
+/*class student {
     constructor(id, name){
         this.id = id;
         this.name = name;
@@ -24,4 +24,55 @@ console.log(s1);
 s1.studentName = "nitsh"
 console.log(s1.name)
 
-console.log(s1.studentInfo);
+console.log(s1.studentInfo);*/
+
+// Synchronous vs Asynchronous----------------------------------------------------------
+const taskOne = () => {
+    console.log("task1");
+};
+const dataloading = () => { //Asynchronous
+    console.log("task2. data loading");
+}
+const task2 = () => {
+   setTimeout(dataloading,2000) ;
+};
+const task3 = () => {
+    console.log("task3");
+};
+const task4 = () => {
+    console.log("task4");
+};
+const task5 = () => {
+    console.log("task5");
+}
+taskOne();
+task2();
+task3();
+task4();
+task5();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
